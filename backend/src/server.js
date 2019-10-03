1fc4a98c41e0a068d879027d00958053446d7690
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2019-10-02 21:58:08 -0300

    [add] Cors API

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.URI_MONGO, {
     useUnifiedTopology: true
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
