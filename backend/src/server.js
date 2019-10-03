f7c1b109fa958366d37673d31009b34194d7f5de
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2019-10-02 22:37:17 -0300

    [add] Retornando as imagens

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.URI_MONGO, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
